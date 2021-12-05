8105590efbb920f2dc1ea0a64e5f24d02bcb8547
TharakaWijesundara <wmtharaka98@gmail.com>
2021-12-06 01:08:01 +0530

    added transfer function

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,10 +2,33 @@
 pragma solidity ^0.6.0;
 
 contract DappToken{
+    string public name = "DApp Token";
+    string public symbol = "DAPP";
+    string public standard = "DApp Token v1.0";
     uint256 public totalSupply;
 
-    constructor() public{
-        totalSupply = 1000000;
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) public{
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success){
+       require(balanceOf[msg.sender] >= _value);
+       balanceOf[msg.sender] -= _value;
+       balanceOf[_to] += _value;
+
+        //check what is transfer before next tutorial
+       emit Transfer(msg.sender, _to, _value);
+
+       return true;
     }
 
 }
