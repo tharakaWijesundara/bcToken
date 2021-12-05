5f718db2e9d74478345a83c5eacb5af27cb5cfc2
TharakaWijesundara <wmtharaka98@gmail.com>
2021-12-05 23:26:05 +0530

    sets total supply

---
+++ contracts/DappToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract DappToken{
+    uint256 public totalSupply;
+
+    constructor() public{
+        totalSupply = 1000000;
+    }
+
+}
